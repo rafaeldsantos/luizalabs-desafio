d94ed0d2a249562280fb1763c95a053da70111eb
Rafael Santos <santosdrafael@gmail.com>
2017-06-03 15:37:41 -0300

    improves error menssage & fix minor error

--- src/webservice.py
+++ src/webservice.py
@@ -14,33 +14,81 @@ def get_friends(username):
     try:
         data = database.get_friends(username)
     except UserNotFound:
-        logging.warning('ERRO 404: {} NOT FOUND'.format(username))
+        logging.warning('ERROR 404: {} NOT FOUND'.format(username))
         data = {'Error':'Username {} not found'.format(username)}
         return jsonify(data=data, meta={"status": "404"})
 
     return jsonify(data=data, meta={"status": "ok"})
 
-@app.route('/sugestions/<username>', methods=['GET'])
-def get_sugestions():
-    logging.info('GET {} SUGGESTIONS'.format(username))
+logging.basicConfig(filename='webservice.log',level=logging.WARNING,format='%(asctime)s %(message)s')
+app = Flask(__name__)
+Swagger(app)
+database = Neo4jManager()
+
+@app.route('/friends/<username>', methods=['GET'])
+def get_friends(username):
+    # """
+    # This is the language awesomeness API
+    # Call this api passing a language name and get back its features
+    # ---
+    # tags:
+    #   - Awesomeness Language API
+    # parameters:
+    #   - name: username
+    #     in: path
+    #     type: string
+    #     required: true
+    # responses:
+    #   404:
+    #     description: User not found!
+    #   200:
+    #     description: A language with its awesomeness
+    #     schema:
+    #       id: awesome
+    #       properties:
+    #         language:
+    #           type: string
+    #           description: The language name
+    #           default: Lua
+    #         features:
+    #           type: array
+    #           description: The awesomeness list
+    #           items:
+    #             type: string
+    #           default: ["perfect", "simple", "lovely"]
+    #
+    # """
+    logging.info('GET {} FRIENDS'.format(username))
     try:
-        data = database.get_suggestion(username)
+        data = database.get_friends(username)
     except UserNotFound:
         logging.warning('ERRO 404: {} NOT FOUND'.format(username))
         data = {'Error':'Username {} not found'.format(username)}
         return jsonify(data=data, meta={"status": "404"})
-
     return jsonify(data=data, meta={"status": "ok"})
+
+@app.route('/sugestions/<username>', methods=['GET'])
+def get_sugestions(username):
+    logging.info('GET {} SUGGESTIONS'.format(username))
+    try:
+        data = database.get_suggestion(username)
+    except UserNotFound:
+        logging.warning('ERROR 404: {} NOT FOUND'.format(username))
+        data = {'Error':'Username {} not found'.format(username)}
+        return jsonify(data=data, meta={"status": "404"})
+    return jsonify(data=data, meta={"status": "ok"})
+
 
 @app.route('/log', methods=['GET'])
 def get_log():
     with open("webservice.log", "r") as content_log:
         log = content_log.read().split('\n')
-    # import pdb;pdb.set_trace()
+
     return jsonify(data=log, meta={"status": "ok"})
 
 if __name__ == '__main__':
     logging.info('STARTING PREPROCESSING...')
     database.preprocessing()
     logging.info('PREPROCESSING END')
-    app.run()
+    app.run(host='0.0.0.0')
+    # app.run()
